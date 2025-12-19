634f188d5e143ff548282df2bf6895ed5318404d
Te <136490142+Tesinazucar@users.noreply.github.com>
2025-12-19 17:52:50 +0100

    Add files via upload

---
+++ Ejercicio2.java
@@ -0,0 +1,19 @@
+package com.mycompany.primerosejercicios;
+public class Ejercicio2 {
+    public static void main (String[] args) {
+        int items = 50;
+        float costPerItem = 9.99f;
+        float totalCost = items * costPerItem;
+        char currency = '$';
+        
+        // print variables
+        System.out.println("Numero de items: " + items);
+        System.out.println("Coste: " + costPerItem);
+        System.out.println("Precio total: " + totalCost + currency);
+        
+        
+        
+        
+    }
+}
+
---
+++ Ejercicio3.java
@@ -0,0 +1,14 @@
+package com.mycompany.primerosejercicios;
+public class Ejercicio3 {
+    public static void main(String[] args) {
+    /* 
+    Calcular el porcentaje del nivel de un jugador respecto al nivel maximo
+        */
+    int maxScore = 500; //el nivel maximo es 500
+    int playerScore = 423; 
+    
+    double porcentaje = (double) playerScore / maxScore * 100.0d ;
+     
+    System.out.println("El porcentaje del jugador es: " + porcentaje);
+    }
+}
