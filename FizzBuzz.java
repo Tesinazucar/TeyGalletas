dee5f17d8c1b1753dc759667c680c38e402b54d3
Te <136490142+Tesinazucar@users.noreply.github.com>
2026-01-31 12:05:09 +0100

    Update FizzBuzz.java

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -1,15 +1,7 @@
 package com.mycompany.primerosejercicios;
 public class FizzBuzz {
 public static void main(String[] args) {
-           /*
-         * Escribe un programa que muestre por consola (con un print) los
-         * números de 1 a 100 (ambos incluidos y con un salto de línea entre
-         * cada impresión), sustituyendo los siguientes:
-         * - Múltiplos de 3 por la palabra "fizz".
-         * - Múltiplos de 5 por la palabra "buzz".
-         * - Múltiplos de 3 y de 5 a la vez por la palabra "fizzbuzz".
-         */
-           
+
        int i;
         for (i = 1; i <= 100; i++) {
             boolean divisibleByThree = i % 3 == 0;
